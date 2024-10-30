06794acafa9d1e770bfebbbe1ec1f92a0ee66e4c
Tjbrowen <tjbrowen@gmail.com>
2024-10-30 17:51:40 +0200

    changed the bacground of success page background

--- client/src/pages/shopping-view/payment-success.jsx
+++ client/src/pages/shopping-view/payment-success.jsx
@@ -6,14 +6,30 @@ function PaymentSuccessPage() {
   const navigate = useNavigate();
 
   return (
-    <Card className="p-10">
-      <CardHeader className="p-0">
-        <CardTitle className="text-4xl">Payment is successfull!</CardTitle>
-      </CardHeader>
-      <Button className="mt-5" onClick={() => navigate("/shop/account")}>
-        Login
-      </Button>
-    </Card>
+    <div
+      className="flex flex-col min-h-screen"
+      style={{
+        backgroundImage: `linear-gradient(
+          rgba(0, 0, 0, 0.5), 
+          rgba(0, 0, 0, 0.5)
+        ), url('https://res.cloudinary.com/daynaexaz/image/upload/v1728893288/blue-smokebg_cegir0.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+      }}
+    >
+      <Card className="p-10 m-auto bg-white/90 shadow-lg rounded-lg">
+        <CardHeader className="p-0">
+          <CardTitle className="text-4xl text-center text-green-500">
+            Payment is successful!
+          </CardTitle>
+        </CardHeader>
+        <Button className="mt-5 w-full" onClick={() => navigate("/shop/account")}>
+          Login
+        </Button>
+      </Card>
+    </div>
   );
 }
 
